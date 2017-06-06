17316a9a15b614d6eef60f9a7ab43a8429b0e898
German <georg.88@mail.ru>
2017-06-06 16:37:55 +0700

    first commit

---
+++ data.h
@@ -0,0 +1,9 @@
+struct child {
+    int id;
+    char name[32];
+};
+
+struct sympathy {
+    int id_from;
+    int id_to;
+};
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+void writeFile() {
+    ofstream fout("name");
+
+}
